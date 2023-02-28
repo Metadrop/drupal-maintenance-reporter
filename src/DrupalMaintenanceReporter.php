382c629833577f9a6800e7703158b24d60c329f2
Omar <omar.lopesino@metadrop.net>
2023-02-28 18:46:15 +0100

    Command to do a full report

---
+++ src/DrupalMaintenanceReporter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DrupalMaintenanceReporting;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DrupalMaintenanceReporter extends BaseCommand {
+  protected static $defaultName = 'report';
+
+  protected function initialize(InputInterface $input, OutputInterface $output)
+  {
+    parent::initialize($input, $output);
+    $this->setDescription('Reports maintenance actions done in a specific period.');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $output->writeln('Composer lock diff:');
+    $this->getApplication()->find('composer-lock-diff-period')
+      ->run($input, $output);
+
+    $output->writeln('Securities fixed:');
+    $this->getApplication()->find('securities-fixed')
+      ->run($input, $output);
+    return 1;
+  }
+
+
+}
