ba385399b2275a6be34f237fd0dfc7128095e9ed
Omar <omar.lopesino@metadrop.net>
2023-03-01 12:02:25 +0100

    Show all drupal fixed securities

--- src/SecuritiesFixedCommand.php
+++ src/SecuritiesFixedCommand.php
@@ -2,13 +2,11 @@
 
 namespace DrupalMaintenanceReporting;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Composer\Semver\Semver;
+use GuzzleHttp\Client;
 
 /**
  * Report composer diff for a specific period.
@@ -21,6 +19,7 @@ class SecuritiesFixedCommand extends BaseCommand {
 
   protected function initialize(InputInterface $input, OutputInterface $output)
   {
+    $this->showSummary($input, $output);
     $this->generateDirSkeleton();
   }
 
@@ -53,6 +52,22 @@ class SecuritiesFixedCommand extends BaseCommand {
     $this->placeFirstCommitJson($from, $branch);
     $this->placeLatestCommitJson($to, $branch);
 
+    $output->writeln("\n");
+    $fixed_advisories_table = $this->getComposerFixedSEcurityAdvisories($to, $output);
+    $output->writeln('Fixed security advisories (Composer):');
+    $fixed_advisories_table->render();
+
+    $output->writeln("\n");
+    $fixed_drupal_advisories_table = $this->getFixedDrupalSecurities($output);
+    $output->writeln('Fixed security advisories (Drupal):');
+    $fixed_drupal_advisories_table->render();
+
+    $this->cleanup();
+    return 1;
+  }
+
+
+  protected function getComposerFixedSEcurityAdvisories(string $to, OutputInterface $output) {
     $from_advisories = $this->getFolderSecurityAdvisoriesByDate($this->getComposerJsonFromLocation(), $to);
     $to_advisories = $this->getFolderSecurityAdvisoriesByDate($this->getComposerJsonToLocation(), $to);
 
@@ -60,14 +75,93 @@ class SecuritiesFixedCommand extends BaseCommand {
       return !array_key_exists($advisory_key, $to_advisories);
     }, ARRAY_FILTER_USE_KEY));
 
-
     $table = new Table($output);
     $table->setHeaders(['Package', 'CVE', 'Link']);
     $table->setRows($fixed_advisories);
-    $table->render();
+    return $table;
+  }
 
-    $this->cleanup();
-    return 1;
+  protected function getFixedDrupalSecurities(OutputInterface $output) {
+    $from_advisories = $this->getFolderDrupalAdvisories($this->getComposerJsonFromLocation());
+    $to_advisories = $this->getFolderDrupalAdvisories($this->getComposerJsonToLocation());
+
+    $fixed_securities =  array_values(array_filter($from_advisories, function ($advisory_key) use ($to_advisories) {
+      return !array_key_exists($advisory_key, $to_advisories);
+    }, ARRAY_FILTER_USE_KEY));
+
+    $composer_lock_to_data = $this->getComposerLockData($this->getComposerJsonToLocation());
+    $fixed_securities = array_filter(array_map(function ($advisory) use ($composer_lock_to_data) {
+      $data = [
+        'name' => $advisory['name'],
+        'from' => $advisory['version'],
+        'to' => '-',
+      ];
+      foreach ($composer_lock_to_data['packages'] as $package) {
+        if ($package['name'] == $advisory['name']) {
+          $data['to'] =  $package['version'];
+          break;
+        }
+      }
+      return $data;
+    }, $fixed_securities));
+
+    $table = new Table($output);
+    $table->setHeaders(['Package', 'From', 'To']);
+    $table->setRows($fixed_securities);
+
+    return $table;
+  }
+
+  protected function getFolderDrupalAdvisories(string $folder) {
+    $composer_lock_data = $this->getComposerLockData($folder);
+    $security_advisories = $this->fetchAdvisoryComposerJson();
+    return $this->calculateSecurityUpdates($composer_lock_data, $security_advisories);
+  }
+
+  /**
+   * Fetches the generated composer.json from drupal-security-advisories.
+   *
+   * @return mixed
+   *
+   * @throws \Exception
+   */
+  protected function fetchAdvisoryComposerJson()
+  {
+    // We use the v2 branch for now, as per https://github.com/drupal-composer/drupal-security-advisories/pull/11.
+    $client = new Client();
+    $response = $client->get('https://raw.githubusercontent.com/drupal-composer/drupal-security-advisories/8.x-v2/composer.json');
+    $security_advisories_composer_json = json_decode($response->getBody(), true);
+    return $security_advisories_composer_json;
+  }
+
+  /**
+   * Return available security updates.
+   *
+   * @param array $composer_lock_data
+   *   The contents of the local Drupal application's composer.lock file.
+   * @param array $security_advisories_composer_json
+   *   The composer.json array from drupal-security-advisories.
+   *
+   * @return array
+   */
+  protected function calculateSecurityUpdates($composer_lock_data, $security_advisories_composer_json)
+  {
+    $updates = [];
+    $packages = $composer_lock_data['packages'];
+    $conflict = $security_advisories_composer_json['conflict'];
+    foreach ($packages as $package) {
+      $name = $package['name'];
+      if ($name == 'drupal/core') {
+        $debug = 1;
+      }
+      if (!empty($conflict[$name]) && Semver::satisfies($package['version'], $security_advisories_composer_json['conflict'][$name])) {
+        $updates[$name] = [
+          'name' => $name,
+          'version' => $package['version'],
+        ];
+      }
+    }
+    return $updates;
   }
 
   protected function getDirMainLocation() {
@@ -83,13 +177,11 @@ class SecuritiesFixedCommand extends BaseCommand {
   }
 
   protected function placeFirstCommitJson(string $date, string $branch) {
-    $first_commit = trim($this->runCommand("git log origin/$branch --after=$date --pretty=format:'%h' | tail -n1")->getOutput());
-    $this->saveComposerCommitStatus($first_commit, $this->getComposerJsonFromLocation());
+    $this->saveComposerCommitStatus($this->getFirstCommit($date, $branch), $this->getComposerJsonFromLocation());
   }
 
   protected function placeLatestCommitJson(string $date, string $branch) {
-    $last_commit = trim($this->runCommand("git log origin/$branch --until=$date --pretty=format:'%h' | head -n1")->getOutput());
-    $this->saveComposerCommitStatus($last_commit, $this->getComposerJsonToLocation());
+    $this->saveComposerCommitStatus($this->getLastCommit($date, $branch), $this->getComposerJsonToLocation());
   }
 
   protected function saveComposerCommitStatus(string $commit_id, string $folder) {
